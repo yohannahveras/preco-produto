e0422ec3e7018f224a6e763c15ec3f8ee6271d5c
Yohannah Véras <yohannahlv@gmail.com>
2025-06-25 11:41:56 -0300

    criada classe ProdutoImportado

--- src/entities/ProdutoImportado.java
+++ src/entities/ProdutoImportado.java
@@ -1,4 +1,31 @@
 package entities;
 
 public class ProdutoImportado extends Produto{
+    private Double taxaAlfandega;
+
+    public ProdutoImportado(String nome, Double preco, Double taxaAlfandega) {
+        super(nome, preco);
+        this.taxaAlfandega = taxaAlfandega;
+    }
+
+    public Double getTaxaAlfandega() {
+        return taxaAlfandega;
+    }
+
+    public void setTaxaAlfandega(Double taxaAlfandega) {
+        this.taxaAlfandega = taxaAlfandega;
+    }
+
+    public Double precoFinal(){
+        return getPreco() + taxaAlfandega;
+    }
+    @Override
+    public String tagPreco(){
+        return getNome()
+                + " R$ "
+                + String.format("%.2f", precoFinal())
+                + " (Taxa de Alfândega: R$ "
+                + String.format("%.2f", taxaAlfandega)
+                + ")";
+    }
 }
